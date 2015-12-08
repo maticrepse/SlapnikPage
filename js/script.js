f9e6f45b2cd3f533bbfd7eda29d1c61cf24b9653
maticrepse <matic.repse@gmail.com>
2015-12-08 10:28:28 +0100

    dodana taba onas in kontakt. pravtako urejena velikost klikanja na navbaru

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	$(".slapnikNav li").click(function(){
+	$(".slapnikNav li a").click(function(){
 		$(".slapnikNav p").toggle();
 		$(".enatriNav p").hide();
 		$(".ullsNav p").hide();
@@ -8,8 +8,10 @@ $(document).ready(function(){
 		$("#slapnikTab").toggle();
 		$("#enatriTab").hide();
 		$("#ullsTab").hide();
+		$("#onasTab").hide();
+		$("#kontaktTab").hide();
 	});
-	$(".enatriNav li").click(function(){
+	$(".enatriNav li a").click(function(){
 		$(".enatriNav p").toggle();
 		$(".slapnikNav p").hide();
 		$(".ullsNav p").hide();
@@ -18,9 +20,12 @@ $(document).ready(function(){
 		//$("#enatriTab").show();
 		$("#enatriTab").toggle();
 		$("#ullsTab").hide();
+		$("#onasTab").hide();
+		$("#kontaktTab").hide();
 	});
-	$(".ullsNav li").click(function(){
+	$(".ullsNav li a").click(function(){
 		$(".ullsNav p").toggle();
+		//$(".ullsNav p").fadeToggle();
 		$(".slapnikNav p").hide();
 		$(".enatriNav p").hide();
 		$("#slapnikTab").hide();
@@ -28,5 +33,31 @@ $(document).ready(function(){
 		//Odkomentiraj spodnjo vrstico in zakomentiraj vrstico pod njo, da bo vedno prikazan en element
 		//$("#ullsTab").show();
 		$("#ullsTab").toggle();
+		$("#onasTab").hide();
+		$("#kontaktTab").hide();
+	});
+	$(".onasNav li a").click(function(){
+		$(".ullsNav p").hide();
+		$(".slapnikNav p").hide();
+		$(".enatriNav p").hide();
+		$("#slapnikTab").hide();
+		$("#enatriTab").hide();
+		$("#ullsTab").hide();
+		//Odkomentiraj spodnjo vrstico in zakomentiraj vrstico pod njo, da bo vedno prikazan en element
+		//$("#onasTab").show();
+		$("#onasTab").toggle();
+		$("#kontaktTab").hide();
+	});
+	$(".kontaktNav li a").click(function(){
+		$(".ullsNav p").hide();
+		$(".slapnikNav p").hide();
+		$(".enatriNav p").hide();
+		$("#slapnikTab").hide();
+		$("#enatriTab").hide();
+		$("#ullsTab").hide();
+		$("#onasTab").hide();
+		//Odkomentiraj spodnjo vrstico in zakomentiraj vrstico pod njo, da bo vedno prikazan en element
+		//$("#kontaktTab").show();
+		$("#kontaktTab").toggle();
 	});
 });
